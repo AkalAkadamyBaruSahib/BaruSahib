601137b58c33a13adbb59cbf18a8c37d9c3f6fcc
yogihub <joshi.yogesh@outlook.com>
2016-07-28 14:41:05 +0530

    changes done for login

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -8,10 +8,9 @@ using System.Data;
 
 public partial class _Default : System.Web.UI.Page
 {
-    private static int? UserTypeID = -1;
     protected void Page_Load(object sender, EventArgs e)
     {
-       
+
     }
 
     protected void btnLogin_Click(object sender, EventArgs e)
@@ -28,84 +27,78 @@ public partial class _Default : System.Web.UI.Page
         {
             UsersRepository repo = new UsersRepository(new AkalAcademy.DataContext());
             Incharge inchrge = new Incharge();
-            //inchrge = repo.GetLoginUserDetail(txtUserName.Text, txtPwd.Text);
-
-            DataSet dsExit = DAL.DalAccessUtility.GetDataInDataSet("select EmailId,Pwd from Login where Active=1 AND EmailId='" + txtUserName.Text + "'");
-            if ((dsExit != null && dsExit.Tables[0] != null && dsExit.Tables[0].Rows.Count > 0) && (dsExit.Tables[0].Rows[0]["EmailId"].ToString() == txtUserName.Text && dsExit.Tables[0].Rows[0]["Pwd"].ToString() == txtPwd.Text))
+            inchrge = repo.GetLoginUserDetail(txtUserName.Text.Trim(), txtPwd.Text.Trim());
+            //DataSet dsExit = DAL.DalAccessUtility.GetDataInDataSet("select EmailId,Pwd from Login where Active=1 AND EmailId='" + txtUserName.Text + "'");
+            if (inchrge != null && inchrge.InchargeId > 0)
             {
-                DataSet ds = new DataSet();
-                ds = DAL.DalAccessUtility.GetDataInDataSet("exec USP_Login '" + txtUserName.Text + "','" + txtPwd.Text + "','" + HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"].ToString() + "'");
-                if (ds.Tables.Count > 0)
-                {
-                    Session["InchargeID"] = ds.Tables[0].Rows[0]["InchargeID"].ToString();
-                    Session["UserTypeID"] = ds.Tables[0].Rows[0]["UserTypeId"].ToString();
-                    Session["UserName"] = ds.Tables[0].Rows[0]["InName"].ToString();
-                    Session["EmailId"] = ds.Tables[0].Rows[0]["EmailId"].ToString();
-                    Session["InName"] = ds.Tables[0].Rows[0]["InName"].ToString();
-                    Session["ModuleID"] = ds.Tables[0].Rows[0]["ModuleID"].ToString();
-                    UserTypeID = Convert.ToInt16(Session["UserTypeID"].ToString());
+                Session["InchargeID"] = inchrge.InchargeId;
+                Session["UserTypeID"] = inchrge.UserTypeId;
+                Session["UserName"] = inchrge.InName;
+                Session["EmailId"] = inchrge.LoginId;
+                Session["InName"] = inchrge.InName;
+                Session["ModuleID"] = inchrge.ModuleID;
 
-                    if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 1 || UserTypeID == 21)
-                    {
-                        Response.Redirect("AdminHome.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 2)
-                    {
-                        Response.Redirect("Emp_Home.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 6)
-                    {
-                        Response.Redirect("Workshop_Home.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 4 || Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 12 || Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 23)
-                    {
-                        Response.Redirect("Purchase_Home.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 3)
-                    {
-                        Response.Redirect("AuditHome.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 5)
-                    {
-                        Response.Redirect("Account_Home.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 7)
-                    {
-                        Response.Redirect("ArchHome.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 10)
-                    {
-                        Response.Redirect("AcademicUserHome.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 9)
-                    {
-                        Response.Redirect("Store_Materials.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 13)
-                    {
-                        Response.Redirect("TransportHome.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 14)
-                    {
-                        Response.Redirect("TransportHome.aspx");
-                    }
-                    else if (Convert.ToInt16(ds.Tables[0].Rows[0]["UserTypeId"].ToString()) == 15 || Session["UserTypeID"].ToString() == "16" || Session["UserTypeID"].ToString() == "17"
-                        || Session["UserTypeID"].ToString() == "18" || Session["UserTypeID"].ToString() == "19" || Session["UserTypeID"].ToString() == "20")
-                    {
-                        Response.Redirect("TransportHome.aspx");
-                    }
-                    else if (UserTypeID == 22)
-                    {
-                        Response.Redirect("Visitor_Home.aspx");
-                    }
-                    else if (UserTypeID == 24)
-                    {
-                        Response.Redirect("Security_Home.aspx");
-                    }
-                    else
-                    {
-                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Please Enter Valid Login Details');", true);
-                    }
+
+                if (inchrge.UserTypeId == 1 || inchrge.UserTypeId == 21)
+                {
+                    Response.Redirect("AdminHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 2)
+                {
+                    Response.Redirect("Emp_Home.aspx");
+                }
+                else if (inchrge.UserTypeId == 6)
+                {
+                    Response.Redirect("Workshop_Home.aspx");
+                }
+                else if (inchrge.UserTypeId == 4 || inchrge.UserTypeId == 12 || inchrge.UserTypeId == 23)
+                {
+                    Response.Redirect("Purchase_Home.aspx");
+                }
+                else if (inchrge.UserTypeId == 3)
+                {
+                    Response.Redirect("AuditHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 5)
+                {
+                    Response.Redirect("Account_Home.aspx");
+                }
+                else if (inchrge.UserTypeId == 7)
+                {
+                    Response.Redirect("ArchHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 10)
+                {
+                    Response.Redirect("AcademicUserHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 9)
+                {
+                    Response.Redirect("Store_Materials.aspx");
+                }
+                else if (inchrge.UserTypeId == 13)
+                {
+                    Response.Redirect("TransportHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 14)
+                {
+                    Response.Redirect("TransportHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 15 || inchrge.UserTypeId == 16 || inchrge.UserTypeId == 17
+                    || inchrge.UserTypeId == 18 || inchrge.UserTypeId == 19 || inchrge.UserTypeId == 20)
+                {
+                    Response.Redirect("TransportHome.aspx");
+                }
+                else if (inchrge.UserTypeId == 22)
+                {
+                    Response.Redirect("Visitor_Home.aspx");
+                }
+                else if (inchrge.UserTypeId == 24)
+                {
+                    Response.Redirect("Security_Home.aspx");
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Please Enter Valid Login Details');", true);
                 }
             }
             else
