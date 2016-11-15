ffbfa38cdabe4651e5764a9d5febad7683e02943
NishuBaruSahib <itmohali@barusahib.org>
2016-11-15 12:05:42 +0530

    Bug Fix In Places according Report

--- Visitors_Reports.aspx.cs
+++ Visitors_Reports.aspx.cs
@@ -185,9 +185,9 @@ public partial class Visitors_Reports : System.Web.UI.Page
     private string getSqlString()
     {
         string sql = string.Empty;
-        sql = "SELECT distinct V.Name AS [Visitor Name], VT.VisitorType,V.ContactNumber,V.VisitorAddress,CO.CountryName,S.StateName,C.CityName,V.PurposeOfVisit,B.Name As BuildingName,RN.BuildingFloor AS BuildingFloor,RN.Number As RoomNumber,V.RoomRent,V.AdmissionNumber, " +
-            "V.VisitorReference,V.VehicleNo,V.Identification,V.TimePeriodFrom As CheckIn,V.TimePeriodTo As CheckOut FROM Visitors V INNER jOIN VisitorRoomNumbers RM ON RM.VisitorID=V.ID INNER JOIN VisitorType VT ON VT.ID= V.VisitorTypeID " +
-            "INNER JOIN BuildingName B ON B.ID= V.BuildingID INNER JOIN RoomNumbers RN ON RM.RoomNumberID = RN.ID INNER JOIN Country CO ON CO.CountryId = V.Country	INNER JOIN State S ON S.StateId = V.State INNER JOIN City C ON C.CityId = V.City WHERE V.VisitorTypeID=" + (int)TypeEnum.VisitoryType.Visitor;
+        sql = "SELECT distinct V.Name AS [Visitor Name], VT.VisitorType,V.ContactNumber,V.VisitorAddress,CO.CountryName,S.StateName,C.CityName,V.PurposeOfVisit,V.RoomRent,V.AdmissionNumber, " +
+            "V.VisitorReference,V.VehicleNo,V.Identification,V.TimePeriodFrom As CheckIn,V.TimePeriodTo As CheckOut FROM Visitors V  INNER JOIN VisitorType VT ON VT.ID= V.VisitorTypeID " +
+            "INNER JOIN Country CO ON CO.CountryId = V.Country	INNER JOIN State S ON S.StateId = V.State INNER JOIN City C ON C.CityId = V.City WHERE V.VisitorTypeID=" + (int)TypeEnum.VisitoryType.Visitor;
 
         if (drpCountry.SelectedValue != "-1")
         {
