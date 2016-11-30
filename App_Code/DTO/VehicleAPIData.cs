6e55c6c91a1321f764d72f92d8b722f4b982bdb7
yogihub <joshi.yogesh@outlook.com>
2016-11-30 20:41:37 +0530

    Fetch GPS data from API

---
+++ App_Code/DTO/VehicleAPIData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for VehicleAPIData
+/// </summary>
+public class VehicleAPIData
+{
+	public VehicleAPIData()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public string vname { get; set; }
+    public string start_datetime { get; set; }
+    public string to_datetime { get; set; }
+    public List<vehicle_attendance_detail> vehicle_attendance_detail { get; set; }
+}
+
+public class vehicle_attendance_detail
+{
+    public string dated { get; set; }
+    public string route_start_km { get; set; }
+    public string school_km { get; set; }
+    public string route_end_km { get; set; }
+    public string attendance { get; set; }
+}
